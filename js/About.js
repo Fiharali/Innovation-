36c057905c8ed93890a312896cf90ccae4b6b1c2
fihar abdelali <fiharali0@gmail.com>
2023-10-17 16:03:23 +0100

    add changes before breaf 3

---
+++ js/About.js
@@ -0,0 +1,8 @@
+menu.onclick = () => {
+  nav.classList.toggle("hide");
+  getLogin.classList.toggle("hide");
+  afterHide.classList.toggle("afterHide");
+  //   setTimeout(function(){
+  // 	afterHide.classList.toggle("afterHide")
+  //    }, 1000);
+};
