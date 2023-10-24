77434b14f9e2c780a95e8a34b0878f388131bd20
fihar abdelali <fiharali0@gmail.com>
2023-10-24 17:03:24 +0100

    add new native slider

--- js/About.js
+++ js/About.js
@@ -3,11 +3,9 @@ hslide1.onclick = function () {
   slideToShow3.classList.add("slideHide");
   slideToShow1.classList.remove("slideHide");
 
-
   hslide1.classList.add("slideDesign");
   hslide2.classList.remove("slideDesign");
   hslide3.classList.remove("slideDesign");
-
 };
 
 hslide2.onclick = function () {
@@ -25,8 +23,48 @@ hslide3.onclick = function () {
   slideToShow1.classList.add("slideHide");
   slideToShow3.classList.remove("slideHide");
 
-
   hslide3.classList.add("slideDesign");
   hslide2.classList.remove("slideDesign");
   hslide1.classList.remove("slideDesign");
 };
+
+//////////////////////////////////////////////////::
+/////////////////native slider////////////////////::
+//////////////////////////////////////////////////::
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const slides = document.querySelectorAll(".superheros .slideNative");
+const totalSlides = slides.length;
+let index = 0;
+//  console.log(totalSlides);
+
+function nextSlide(dir) {
+  if (dir == "next") {
+    index++;
+    if (index == totalSlides) {
+      index = 0;
+    } else {
+      //
+    }
+  } else {
+    if (index == 0) {
+      index = totalSlides - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slides[index].classList.add("active");
+}
+
+prev.addEventListener("click", () => {
+  nextSlide("next");
+  console.log("jfsv");
+});
+
+next.addEventListener("click", () => {
+  nextSlide("prev");
+});
--- js/main.js
+++ js/main.js
@@ -4,5 +4,13 @@ menu.onclick = () => {
   afterHide.classList.toggle("afterHide");
 };
 
-
-
+let items = document.querySelectorAll("#nav > ul > li");
+// console.log(items)
+items.forEach((item) => {
+  item.onclick = () => {
+    items.forEach((navItem) => {
+      navItem.classList.remove("navStyle");
+    });
+    item.classList.add("navStyle");
+  };
+});
